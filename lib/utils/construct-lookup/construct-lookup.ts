870c4a2da2a9618503e1972df732db553b95de75
cyfo <codeyourfaceoff@gmail.com>
2023-04-17 21:00:44 -0500

    fix(construct-lookup): keep stack until looked up
    we can't remove the stack until the construct is actually looked up from either the cdk.context.json or the sdk calls.
    
    the only check at the moment is to see if one of the Dummy constructs was ever loaded

--- lib/utils/construct-lookup/construct-lookup.ts
+++ lib/utils/construct-lookup/construct-lookup.ts
@@ -10,10 +10,33 @@ export async function constructLookup<LookupHandler extends AnyFunction>(
   const stack = await stackFromProfile(app, props.profile);
   const construct = props.lookupHandler(stack, id, props.lookupProps);
 
-  // Remove the stack so it's not a part of the deployment
-  app.node.tryRemoveChild(stack.node.id);
+  // the cdk defaults to 'dummy' constructs before doing the actual lookup
+  // we need keep the parent stack around until it finally resolves the construct we want
+  // this should be once per env as the cdk will cache the values in cdk.context.json
+  if (!isDummyLookup(construct)) {
+    // Remove the stack so it's not a part of the deployment
+    // that way we don't have to specify which stack to deploy if only really doing 1 stack
+    app.node.tryRemoveChild(stack.node.id);
+  }
 
   return construct;
+
+  function isDummyLookup(construct: any) {
+    return JSON.stringify(construct, getCircularReplacer()).includes("DUMMY");
+  }
+
+  function getCircularReplacer() {
+    const seen = new WeakSet();
+    return (key: string, value: any) => {
+      if (typeof value === "object" && value !== null) {
+        if (seen.has(value)) {
+          return;
+        }
+        seen.add(value);
+      }
+      return value;
+    };
+  }
 }
 
 export interface ConstructLookupProps<LookupHandler extends AnyFunction> {
