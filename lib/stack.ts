14618dc27082c2a04ddaa080e1dcec37b701af48
cyfo <codeyourfaceoff@gmail.com>
2023-04-17 21:00:43 -0500

    refactor(utils): move `simple-stack` into sub dir

--- lib/stack.ts
+++ lib/stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from "aws-cdk-lib";
 import * as constructs from "constructs";
-import { SimpleStack } from './simple-stack'
+import { SimpleStack } from './utils'
 
 export class Stack extends SimpleStack {
   constructor(scope: constructs.Construct, id: string, props?: cdk.StackProps) {
